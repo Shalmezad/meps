50c21df17e30e18b267eeea698486e37d1e4f020
Richard Wardin <shalmezad@gmail.com>
2014-11-21 09:43:59 -0500

    Player movement

--- source/Player.hx
+++ source/Player.hx
@@ -1,12 +1,41 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.FlxG;
 
 class Player extends FlxSprite
 {
+
+  var SPEED:Int = 30;
+
   public function new()
   {
     super();
     makeGraphic(10,10,0xFFFFFF00);
+  }
+
+  override public function update():Void
+  {
+    super.update();
+
+    velocity.x = 0;
+    velocity.y = 0;
+
+    if(FlxG.keys.pressed.W)
+    {
+      velocity.y -= SPEED; 
+    }
+    if(FlxG.keys.pressed.S)
+    {
+      velocity.y += SPEED;
+    }
+    if(FlxG.keys.pressed.A)
+    {
+      velocity.x -= SPEED;
+    }
+    if(FlxG.keys.pressed.D)
+    {
+      velocity.x += SPEED;
+    }
   }
 }
